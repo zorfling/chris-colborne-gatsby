2bee645bf8268457e17cdf9fe3dcebf6e96a34c3
Chris Colborne <zorfling@gmail.com>
2020-12-26 22:35:46 +1000

    Fix RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,7 @@ module.exports = {
                 title
                 description
                 siteUrl
+                site_url: siteUrl
               }
             }
           }
@@ -87,7 +88,7 @@ module.exports = {
             serialize: ({ query: { site, allMdx } }) => {
               return allMdx.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.description,
+                  description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
