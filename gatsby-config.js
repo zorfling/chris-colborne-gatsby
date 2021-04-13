c72e78a9d51500e1d1f18c0ced8823aff6b8c45f
Chris Colborne <zorfling@gmail.com>
2021-04-13 19:29:23 +1000

    Add amplitude

--- gatsby-config.js
+++ gatsby-config.js
@@ -217,5 +217,31 @@ module.exports = {
         tracesSampleRate: 1,
       },
     },
+    {
+      resolve: `gatsby-plugin-amplitude-analytics`,
+      options: {
+        // Specify the API key for your Amplitude Project (required)
+        apiKey: '2b6c6ca8e11a7afca23e5929a535b646',
+        // Puts tracking script in the head instead of the body (optional)
+        head: false,
+        // Prevents loading Amplitude and logging events if visitors have "Do Not Track" enabled (optional)
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths (optional)
+        // exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Override the default event types (optional)
+        eventTypes: {
+          outboundLinkClick: 'OUTBOUND_LINK_CLICK',
+          pageView: 'PAGE_VIEW',
+        },
+        // Amplitude JS SDK configuration options (optional)
+        amplitudeConfig: {
+          saveEvents: true,
+          includeUtm: true,
+          includeReferrer: true,
+        },
+        // Specify NODE_ENVs in which the plugin should be loaded (optional)
+        environments: ['production'],
+      },
+    },
   ],
 };
